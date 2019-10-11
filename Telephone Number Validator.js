3073bc705adf75e5b2252e9e3a33a7a4c3809543
Rajneesh Singh <39029789+Rajneesh-Singh@users.noreply.github.com>
2019-10-11 23:50:11 +0530

    Telephone Number Validator

---
+++ Telephone Number Validator.js
@@ -0,0 +1,8 @@
+function telephoneCheck(str) {
+    // Good luck!
+  let r = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    
+    return r.test(str);
+  }
+  
+  telephoneCheck("555-555-5555");
